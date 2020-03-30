a969c3af07ec5dd6a50205f09aa4edbc0f23448a
womp-womp <nfernandez011305@gmail.com>
2020-03-30 17:40:05 +0000

    Added Backstory function

--- bot.py
+++ bot.py
@@ -23,7 +23,11 @@ async def say_hello(ctx):
 async def sacrifice(ctx, x):
     opt = ["Yummy", "Nom", "Thank you", "*screeching*", "Mort is thankful", "I need more", "Scrumptious", "UWU"]
     await ctx.send(opt[random.randint(0, 7)])
-    
+
+@bot.command(name="Backstory", help="Just type this command to learn the lore behind Mort")
+async def backstory(ctx):
+    await ctx.send("Long ago, in the very center of the Earth, a single rock of magma and hot gases combined with an unseen power, creating an ominpotent being of mass-destruction. Feared by all, the beast was contained in an ancient form of hydroflask by the first VSCO girls. They worshipped the being from the hydroflask for centuries, until a tourist accidentally opened the bottle. The beast arose from it's prison of plastic, taking form. The beast began it's destructive course. After decades of hiding, a single human was able to tame the beast. Over time, it developed a body to control, that of a small lemur. The body allowed the beast to communicate with humans. The being was further tamed to solve mathematical equations and now spends it's days solving the problems of any humans that give sacrifices. He likes discord and spends most of his time there, where he set up a service for helping humans with math. The creature was Mort.")
+
 bot.run(TOKEN)
  #random stuff
 
