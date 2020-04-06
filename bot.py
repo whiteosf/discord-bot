b5d03dbed7b0562ffce8a63eb9a16e465999e23b
whiteosf <olkabuglak@gmail.com>
2020-04-06 23:38:37 +0000

    Dealed with imaginaries

--- bot.py
+++ bot.py
@@ -21,13 +21,16 @@ async def on_ready():
 async def say_hello(ctx):
     await ctx.send(f"Hi {ctx.author.display_name}")
 
-@bot.command(name = "equ", help="Solves your sacrifised equations")
+@bot.command(name = "equ", help="Solves your sacrifised quadratic and linear equations, takes in three arguments: a, b, c, which are coefficients. Even if coefficient is 0, it has to be listed. As example for equation 3x-4=0 you have to send 0 3 -4 as argumants.")
 async def equation(ctx, a, b, c):
     if a==0:
         await ctx.send(f"x = {(-c)/b}")
     elif a!=0:
         discr= b**2 - 4*a*c
-        await ctx.send(f"x = {[(-b+math.sqrt(discr))/2*a, (-b-math.sqrt(discr))/2*a]}")
+        if discr>=0:
+            await ctx.send(f"x = {(-b+math.sqrt(discr))/2*a} ,  {(-b-math.sqrt(discr))/2*a]}")
+        else:
+            await ctx.send(f"x = ({-b} ± i√{discr})/{2*a}")
     elif a==0 & b==0:
         await ctx.send("There is no x to solve for")
 
