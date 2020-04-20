78b493b868ba3e81a24f63c3c2867c083670598b
whiteosf <olkabuglak@gmail.com>
2020-04-20 22:45:20 +0000

    sdf

--- bot.py
+++ bot.py
@@ -110,7 +110,7 @@ async def linear_systems(ctx, a:int, b:int, c:int, d:int):
     await ctx.send(f"Your x value is equal to {x} and your y value is equal to {y}")
     
 @bot.command("add_subtract", help = "takes values that you want to add and subtract. eg. putting in 12, -10, 1 would give you 3.")
-async def add_and_subtract(ctx, *args):
+async def add_and_subtract(ctx, *args:float):
     if args_check(*args)==True:
         await ctx.send(sum(*args))
     else:
