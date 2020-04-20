30644d1dc58ff6a861c681da2b59641be0a07538
whiteosf <60788417+whiteosf@users.noreply.github.com>
2020-04-19 22:21:29 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -96,7 +96,7 @@ async def add_and_subtract(ctx, *args):
     await ctx.send(sum(*args))
     
 @bot.command(name = "multiply", help= "multiplies two values")
-async def multiply(ctx, x,y):
+async def multiply(ctx,int:x,int:y):
     await ctx.send(x*y)
 
 @bot.command(name = "divide", help = "divides two values")
